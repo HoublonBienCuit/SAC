a7b52cb24298c8c1d438cfa2711a03b7ae759afc
Daniel Boisclair <1836791@cegeprdl.ca>
2021-08-24 16:14:31 -0400

    Test BDD

--- service/server.js
+++ service/server.js
@@ -1,14 +1,30 @@
 require("dotenv").config();
 
 const express = require('express');
+const mysql = require("mysql");
 const app = express();
 
 // Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORTTEST_T || 8080;
+const PORT = process.env.APP_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+const pool = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: "localhost"
+});
+
+app.get('/:bois', (req, res) => {
+  const query = "SELECT * FROM users WHERE ";
+  pool.query(query, [req.params.bois], (error, results) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(results);
+    }
+    
+  })
 });
